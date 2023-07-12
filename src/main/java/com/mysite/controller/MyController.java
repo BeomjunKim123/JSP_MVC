1a1500fa7d734f344cfb0c2fb276af7862141ba4
Beomjun Kim <gkgk9753@gmail.com>
2023-07-12 18:16:01 +0900

    JSP_MVC 2023.07.12

--- src/main/java/com/mysite/controller/MyController.java
+++ src/main/java/com/mysite/controller/MyController.java
@@ -117,9 +117,43 @@ public class MyController extends HttpServlet {
 			
 		} else if (path.equals("/getBoard.do")) {
 			System.out.println("getBoard.do 를 요청 했습니다. ");
-			// 게시판의 값을 읽어 올때 
+			// 게시판의 값을 읽어 올때 (글 상세)
+			
+			//request.getParameter 으로 넘어오는 모든 변수의 값은 String
+			// Spring 에서는 자동으로 Framework에서 자동으로 처리됨 
+			
+				//Integer.parseInt(seq) 
+			
+			String seq = request.getParameter("seq");
+			
+			//1. DTO 에 seq 의 값을 setter 주입 
+			BoardDTO dto = new BoardDTO(); 
+			dto.setSeq(Integer.parseInt(seq));
+			
+			//3. DAO 객체에 getBoard(dto) 넣어서 호출 ====> 리턴으로 dto 를 받아온다. 
+			BoardDAO dao = new BoardDAO(); 
+			
+			//리턴으로 돌아오는 DTO 값을 받을 변수 선언 
+			BoardDTO board = new BoardDTO(); 
+			board = dao.getBoard(dto); 
+			
+			// System.out.println(board);
+			
+			
+			//4. session 에 값을 저장후 뷰 페이지로 전달 : Model 
+				// 현재 클라이언트가 서버에 접속한 세션을 가지오 옴 
+			
+			// 세션 변수 선언 
+			HttpSession session = request.getSession(); 
+			
+			//세션 에 변수에 DB에서 select 한 DTO를 저장후 뷰 페이지로 전송 
+			session.setAttribute("board", board); 
+			
+			response.sendRedirect("getBoard.jsp"); 
+			
 			
 		} else if (path.equals("/getBoardList.do")) {
+			// 글 목록 
 			System.out.println("getBoardList.do 를 요청 했습니다. ");
 			
 			//1. DTO 객체 생성 
@@ -191,6 +225,40 @@ public class MyController extends HttpServlet {
 			
 			//4. 비즈니스로직 모두 처리후 view 페이지 이동 
 			response.sendRedirect("getUsersList.jsp"); 
+			
+			
+			//글 수정 
+		}else if (path.equals("/updateBoard.do")) {
+			//
+			System.out.println("/updateBoard.do - 요청 성공 ");
+			
+			//1. 클라이언트에서 넘어오는 변수 값을 받음. 
+			String title = request.getParameter("title"); 
+			String content = request.getParameter("content"); 
+			String seq = request.getParameter("seq"); 
+			
+			System.out.println("넘버 변수 변환 : " + seq);
+			
+			//2. DTO에 Setter 주입 
+			BoardDTO dto = new BoardDTO(); 
+			dto.setTitle(title); 
+			dto.setContent(content); 
+			dto.setSeq(Integer.parseInt(seq)); 
+			
+			//3. DAO의 메소드 호출 : updateBoard(dto) 
+			BoardDAO dao = new BoardDAO(); 
+			dao.updateBoard(dto); 
+			
+			// 4. view 페이지로 이동 
+			
+			response.sendRedirect("getBoardList.do"); 
+			
+			
+			
+			
+			
+			
+			
 		}
 			
 		
--- src/main/webapp/Oracle_Script.sql
+++ src/main/webapp/Oracle_Script.sql
@@ -1,34 +1,50 @@
+drop table board; 
+
 create table board (
-    seq number(5) not null primary key,
-    title varchar2(200) null,
-    writer varchar(20) null,
+    seq number(5) not null primary key, 
+    title varchar2(200) null , 
+    write varchar2(20) null, 
     content varchar2(2000) null,
-    regdate date default sysdate null,
-    cnt number (5)
-    );
+    regdate date default sysdate null , 
+    cnt number(5)  default 0 null 
+    ) ; 
+  
+insert into board 
+values ( 1, 'MVC M2 게시판 제목', 'admin', 'MVC M2 게시판 내용', default, 0); 
+    
+insert into board 
+values ( 2, 'MVC M2 게시판 제목2', 'user', 'MVC M2 게시판 내용2', default, 0); 
+commit; 
+
+-- sql 에서 commit을 하지 않는 경우; 무한정 대기상태에 빠진다. (LOCK) 
+insert into board (seq, title, write, content) 
+values ( 4 , '글제목 4', 'admin', '글내용 4');
+
+
+
+
+select * from board
+order by seq desc ; 
     
 
-    
-insert into board
-values (1, 'MVC M2 게시판 제목', 'admin', 'MVC M2 게시판 내용', default, 0 );
-
-insert into board
-values (2, 'MVC M2 게시판 제목2', 'admin', 'MVC M2 게시판 내용2', default, 0 );
-
-select * from board;
-
-create table users ( 
-    id varchar2(8) not null primary key,
-    password varchar2(8) null,
-    name varchar2(20) null,
+create table users (
+    id varchar2(8) not null primary key, 
+    password varchar2(8) null ,
+    name varchar2(20) null, 
     role varchar2(5)
-    );
+    ) ; 
     
--- 더미 데이터 입력
-insert into users
-values ('admin', '1234', '관리자', 'Admin');
+    
+-- 더미 데이터 입력     
+insert into users 
+values ( 'admin', '1234', '관리자' , 'Admin');  
 
-insert into users
-values('user', '1234', '일반사용자', 'Users');
+insert into users 
+values ( 'user', '1234', '일반사용자', 'Users') ; 
+commit ; 
+select * from users; 
 
-select * from users;
+
+
+select nvl(max(seq),0) + 1 from board
+    
